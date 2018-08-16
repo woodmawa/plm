dfc25ce10a2a55c3b949a3eb1de43feafb76d799
woodmawa <will.woodman@btinternet.com>
2018-08-16 08:41:22 +0100

    converted Application class to Singleton and restructured the bootstrap folder

--- src/main/groovy/com/softwood/portfolio/Product.groovy
+++ src/main/groovy/com/softwood/portfolio/Product.groovy
@@ -22,6 +22,8 @@ class Product {
     ProductClass productClass
     ConcurrentLinkedQueue productLines = new ConcurrentLinkedQueue()
     ConcurrentLinkedQueue productCapability = new ConcurrentLinkedQueue()
+    ProductAttributeAssignment attributeAssignment
+
 
     //owning end is with ProductLine
     void addProductLine (ProductLine pl) {
@@ -41,6 +43,18 @@ class Product {
     void removeProductCapability (ProductCapability pc) {
         assert pc
         productCapability.remove()
+    }
+
+    //sort of optional - you can always do query on assigments for matched product
+    //but this permits optimised read of attribute assignments for this product directly
+    //from the product itself - use groovy setter/getter for now
+    /*void setAttributeAssignment (ProductAttributeAssignment attributeMapping) {
+        attributeAssignment = attributeMapping
+    }*/
+
+    ArrayList getAttributesSummary () {
+
+        
     }
 
     String toString() {
